0b74b90f102d4be86c88b8ef93f4b20a33c1ace2
Tobias Murphy <murphy.tobias@gmail.com>
2017-01-17 22:28:07 -0800

    works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 /* setup your angular app here */
+console.log('app.js was loaded.');
 
-// debug stuff to show the app is loading and fruit / veggies are available
+angular.module('App', [])
+.controller('appCtrl', ['$scope', function($scope){
+
+
+
+var fruits = [
+  'Apple',
+  'Apricot',
+  'Banana'
+];
+
+var vegetables = [
+  'Artichoke',
+  'Arugula',
+  'Asparagus'
+];
+
+$scope.foods = fruits.concat(vegetables);
+$scope.vegs = [];
+$scope.froots = [];
+
+
+
+$scope.click = function(destination, departure, index) {
+    $scope[destination].push($scope[departure][index]);
+    $scope[departure].splice(index, 1);
+  };
+
+
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+console.log($scope.foods);
+}]);
